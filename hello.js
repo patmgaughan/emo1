8020fa6b3d8882af172d65daee567da7cf34617e
patmgaughan <71145712+patmgaughan@users.noreply.github.com>
2020-12-07 19:50:55 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 3000;
 console.log("This goes to the console window");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.write("<h1>Stock Ticker</h1>");
+   res.write("<h1>Stock Ticker</h1>"); 
    res.write ("<form>");
   res.write ("Search: <input type='text'>");
   res.write ("</form>");
